b4817565eb37b65b08981b37a315378c003d2c38
kxzhu <zhukexin1993@qq.com>
2025-05-18 23:07:18 -0400

    add multiple channels and customize channel preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:stream_chat_flutter/stream_chat_flutter.dart';
+import 'package:collection/collection.dart';
 
 void main() async {
   /// Create a new instance of [StreamChatClient] passing the apikey obtained from your
@@ -18,20 +19,20 @@ void main() async {
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidHV0b3JpYWwtZmx1dHRlciJ9.S-MJpoSwDiqyXpUURgO5wVqJ4vKlIVFLSEyrFYCOE1c',
   );
 
-  /// Creates a channel using the type `messaging` and `flutterdevs`.
-  /// Channels are containers for holding messages between different members. To
-  /// learn more about channels and some of our predefined types, checkout our
+  /// Creates a channel using the type `messaging` and `flutterdevs`. (When handling one channel)
+  /// Channels are containers for holding messages between different members.
+  /// To learn more about channels and some of our predefined types, checkout our
   /// our channel docs: https://getstream.io/chat/docs/flutter-dart/creating_channels/?language=dart
-  final channel = client.channel('messaging', id: 'flutterdevs');
+  // final channel = client.channel('messaging', id: 'flutterdevs');
 
-  /// `.watch()` is used to create and listen to the channel for updates. If the
-  /// channel already exists, it will simply listen for new events.
-  await channel.watch();
+  /// `.watch()` is used to create and listen to the channel for updates. (When handling one channel)
+  /// If the channel already exists, it will simply listen for new events.
+  // await channel.watch();
 
   runApp(
     MyApp(
       client: client,
-      channel: channel,
+      // channel: channel, // (When handling one channel)
     ),
   );
 }
@@ -41,29 +42,42 @@ class MyApp extends StatelessWidget {
   const MyApp({
     Key? key,
     required this.client,
-    required this.channel,
+    // required this.channel, // (When handling one channel)
   }) : super(key: key);
 
   /// Instance of [StreamChatClient] we created earlier. This contains information about
   /// our application and connection state.
   final StreamChatClient client;
 
-  /// The channel we'd like to observe and participate.
-  final Channel channel;
+  /// The channel we'd like to observe and participate.  (When handling one channel)
+  // final Channel channel;
 
+  //  (When handling one channel):
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     builder: (context, widget) {
+  //       return StreamChat(
+  //         client: client,
+  //         child: widget,
+  //       );
+  //     },
+  //     home: StreamChannel(
+  //       channel: channel,
+  //       child: const ChannelPage(),
+  //     ),
+  //   );
+  // }
+
+  //  (When handling multiple channels)
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      builder: (context, widget) {
-        return StreamChat(
-          client: client,
-          child: widget,
-        );
-      },
-      home: StreamChannel(
-        channel: channel,
-        child: const ChannelPage(),
+      builder: (context, child) => StreamChat(
+        client: client,
+        child: child,
       ),
+      home: const ChannelListPage(),
     );
   }
 }
@@ -89,3 +103,98 @@ class ChannelPage extends StatelessWidget {
     );
   }
 }
+
+//  (When handling multiple channels):
+class ChannelListPage extends StatefulWidget {
+  const ChannelListPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<ChannelListPage> createState() => _ChannelListPageState();
+}
+
+class _ChannelListPageState extends State<ChannelListPage> {
+  late final _listController = StreamChannelListController(
+    client: StreamChat.of(context).client,
+    filter: Filter.in_(
+      'members',
+      [StreamChat.of(context).currentUser!.id],
+    ),
+    channelStateSort: const [SortOption('last_message_at')],
+    limit: 20,
+  );
+
+  @override
+  void dispose() {
+    _listController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamChannelListView(
+        controller: _listController,
+        itemBuilder: _channelTileBuilder,
+        onChannelTap: (channel) {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) {
+                return StreamChannel(
+                  channel: channel,
+                  child: const ChannelPage(),
+                );
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _channelTileBuilder(BuildContext context, List<Channel> channels,
+      int index, StreamChannelListTile defaultChannelTile) {
+    final channel = channels[index];
+    final lastMessage = channel.state?.messages.reversed.firstWhereOrNull(
+          (message) => !message.isDeleted,
+    );
+
+    final subtitle = lastMessage == null ? 'nothing yet' : lastMessage.text!;
+    final opacity = (channel.state?.unreadCount ?? 0) > 0 ? 1.0 : 0.5;
+
+    final theme = StreamChatTheme.of(context);
+
+    return ListTile(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => StreamChannel(
+              channel: channel,
+              child: const ChannelPage(),
+            ),
+          ),
+        );
+      },
+      leading: StreamChannelAvatar(
+        channel: channel,
+      ),
+      title: StreamChannelName(
+        channel: channel,
+        textStyle: theme.channelPreviewTheme.titleStyle!.copyWith(
+          color: theme.colorTheme.textHighEmphasis.withOpacity(opacity),
+        ),
+      ),
+      subtitle: Text(subtitle),
+      trailing: channel.state!.unreadCount > 0
+          ? CircleAvatar(
+        radius: 10,
+        child: Text(channel.state!.unreadCount.toString()),
+      )
+          : const SizedBox(),
+    );
+  }
+}
+
+
